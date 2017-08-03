700b150172e4f707ac6763c54b2e4aa626eaf06c
ChangLiuuu <spiritchang@hotmail.com>
2017-08-03 11:38:52 -0700

    parse url
    parse url : 1. query string  2. url

--- 4. get-demo.js
+++ 4. get-demo.js
@@ -7,9 +7,73 @@
 
 //请求方式：1. GET : 数据在url中 2. POST：数据不在url中
 
+    // 使用querystring parse URL
 const http = require('http');
-const url_lib = require('url');
+const quarystring = require('querystring');
 var server = http.createServer(function (req, res) {
-    console.log(req.url);
+    var GET = {};
+    var urladd;
+    if (req.url.indexOf('?') !== -1) {
+        var arr = req.url.split('?');
+        urladd = arr[0];
+        GET = quarystring.parse(arr[1]);
+
+    } else {
+        urladd = req.url;
+    }
+    console.log(urladd ,'-----' , GET);
     res.write('aaa');
+    res.end();
+})
+    // .listen(8080);
+
+    //使用url parse URL
+const urlLib = require('url');
+var obj = urlLib.parse("http://www.baidu.com/index?name=changchang&password=vvc");
+
+var obj1 = urlLib.parse("http://www.baidu.com/index?name=changchang&password=vvc", true);
+
+/* 给true：    query就分割成了JSON: { name: 'changchang', password: 'vvc' },
+  Url {
+  protocol: 'http:',
+  slashes: true,
+  auth: null,
+  host: 'www.baidu.com',
+  port: null,
+  hostname: 'www.baidu.com',
+  hash: null,
+  search: '?name=changchang&password=vvc',
+query: 'name=changchang&password=vvc',
+  pathname: '/index',
+  path: '/index?name=changchang&password=vvc',
+  href: 'http://www.baidu.com/index?name=changchang&password=vvc' }
+ */
+
+var urladd = obj1.pathname;
+var GET = obj1.query;
+
+//console.log(urladd, GET);
+
+var server1 = http.createServer(function (req, res) {
+
+    var obj2 = urlLib.parse(req.url,true);
+    var GET = obj2.query;
+    console.log(GET);
 }).listen(8080);
+// const http = require('http');
+// const urlLib = require('url');
+// var server = http.createServer(function (req, res) {
+//     var GET = {};
+//     var urladd;
+//     if (req.url.indexOf('?') !== -1) {
+//         var arr = req.url.split('?');
+//         urladd = arr[0];
+//         GET = quarystring.parse(arr[1]);
+//
+//     } else {
+//         urladd = req.url;
+//     }
+//     console.log(urladd ,'-----' , GET);
+//     res.write('aaa');
+//     res.end();
+// })
